437d6a1d40938eda1ece688a845c4ad39d313280
saga0619 <june992@gmail.com>
2024-09-03 14:27:19 +0900

    priority and safety

--- src/master_lower.cpp
+++ src/master_lower.cpp
@@ -76,7 +76,7 @@ int main(int argc, char **argv)
         printf("pthread setschedpolicy failed\n");
         return ret;
     }
-    param.sched_priority = 46+50;
+    param.sched_priority = 46+20;
     ret = pthread_attr_setschedparam(&attr, &param);
     if (ret)
     {
